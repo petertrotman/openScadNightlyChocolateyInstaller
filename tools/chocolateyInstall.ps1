e2b0577426c8b2b49109fa123d6d2a27fa401dbe
Peter Trotman <petertrotman@gmail.com>
2022-10-16 17:13:15 +0100

    Update chocolateyInstall.ps1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'openscad' 
-$url = 'https://files.openscad.org/OpenSCAD-2019.05-x86-32-Installer.exe' 
-$url64 = 'https://files.openscad.org/OpenSCAD-2019.05-x86-64-Installer.exe' 
-$checksum = 'c1651c5eadcef47087240a3d03d4dae7e8d99977e7053187f704b2b0a1275297'
-$checksum64 = '4b8b42187f2cbc450951f2321e82f300d4302822bbd35a0bd3878bf5957138a6'
+$url = 'https://files.openscad.org/snapshots/OpenSCAD-2022.10.15.ci12391-x86-32-Installer.exe' 
+$url64 = 'https://files.openscad.org/snapshots/OpenSCAD-2022.10.15.ci12392-x86-64-Installer.exe' 
+$checksum = 'd44e718d1016126a16541deacd78f9386925b99a01725aeea5587c69219c18f0'
+$checksum64 = 'dac7a4bb567204e88800e36df087654709609a001e71f9976095d4268d0cf0e3'
 Install-ChocolateyPackage "$packageName" "exe" "/S" "$url" "$url64" -checksum $checksum -ChecksumType 'sha256' -checksum64 $checksum64 -ChecksumType64 'sha256' -UseOnlyPackageSilentArguments
